ebade797f2cd22556889a861a508b292a804a473
Damian <damwinkler@icloud.com>
2025-10-23 07:49:05 +0200

    update button to accept color prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,24 @@ interface ButtonProps {
   children: React.ReactNode;
   disabled?: boolean;
   onClick?: () => void;
+  color?: "bg-primary" | "bg-red-500";
 }
 
-export default function Button({ children, disabled, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  disabled,
+  onClick,
+  color = "bg-primary",
+}: ButtonProps) {
+  const hoverMap = {
+    "bg-primary": "hover:bg-primary/80",
+    "bg-red-500": "hover:bg-red-500/80",
+  };
   return (
     <button
       type="button"
       disabled={disabled}
-      className="text-sm lg:text-base bg-primary text-white font-medium rounded-md px-4 py-2 cursor-pointer transition-opacity hover:bg-primary/80 disabled:opacity-50"
+      className={`text-sm lg:text-base ${color} text-white font-medium rounded-md px-4 py-2 cursor-pointer transition-opacity ${hoverMap[color]} disabled:opacity-50`}
       onClick={onClick}
     >
       {children}
