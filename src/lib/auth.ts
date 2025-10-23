229b3b502514be7aeb82e36e78e1d45b8045aa00
Damian <damwinkler@icloud.com>
2025-10-23 09:30:38 +0200

    handle auth errors by redirecting to home

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,8 +6,17 @@ export const authOptions: AuthOptions = {
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      // authorization: {
+      //   params: {
+      //     prompt: "consent select_account",
+      //   },
+      // },
     }),
   ],
+  pages: {
+    signIn: "/",
+    error: "/",
+  },
 };
 
 export default NextAuth(authOptions);
