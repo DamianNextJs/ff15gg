b42dc6fa648ed3904343ec79459675157fe34d2c
Damian <damwinkler@icloud.com>
2025-10-23 09:30:46 +0200

    add reusable modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  title?: string;
+  children: React.ReactNode;
+}
+
+export default function Modal({ open, onClose, title, children }: ModalProps) {
+  if (!open) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4">
+      <div className="bg-secondary rounded-md p-4 w-sm  flex flex-col gap-3">
+        <div className="flex items-center justify-between">
+          {title && <h3 className="text-lg font-medium">{title}</h3>}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 cursor-pointer"
+            onClick={onClose}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
