1f2f2a10e01ebcb961767d999558407dec2c781d
Damian <damwinkler@icloud.com>
2025-10-24 12:41:37 +0200

    fix: use h-dvh for SidebarDrawer to prevent UserCard overlap on mobile

--- src/components/sidebar/SidebarDrawer.tsx
+++ src/components/sidebar/SidebarDrawer.tsx
@@ -6,7 +6,7 @@ export default function SidebarDrawer() {
   const { isOpen } = useSidebarDrawer();
   return (
     <div
-      className={`w-full lg:w-64 p-4 h-screen bg-accent absolute left-0 top-0 mt-16 border-t-2 shadow shadow-black border-secondary transition-transform duration-200
+      className={`w-full lg:w-64 p-4 h-dvh bg-accent absolute left-0 top-0 mt-16 border-t-2 shadow shadow-black border-secondary transition-transform duration-200
          ease-in-out ${
            isOpen ? "translate-x-0" : "-translate-x-full lg:-translate-x-64"
          }`}
